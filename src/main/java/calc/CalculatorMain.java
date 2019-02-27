667368e295e0670ebc16089ae097ceabb952ebff
Robin Westerlund <robin_westerlund@hotmail.com>
2019-02-27 10:13:19 +0100

    Added functions returning 0 to demonstrate the test cases failing.

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,4 +7,13 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	public int sub(int a, int b) {
+		return 0;
+	}
+	public int div(int a, int b) {
+		return 0;
+	}
+	public int mul(int a, int b) {
+		return 0;
+	}
 }
